2541a6cacf0fbd3ff902fd2a423aa70485459ffc
Push5875 <mhatrepushpak00@gmail.com>
2024-07-03 12:32:59 +0530

    Dockerzied application

--- app.py
+++ app.py
@@ -11,4 +11,5 @@ def about():
     return jsonify({"version": "1.0", "author": "Yagmur Ozden"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # here app is listening to any host Ip and port 5000 
+    app.run(debug=True, host='0.0.0.0', port=5000)
